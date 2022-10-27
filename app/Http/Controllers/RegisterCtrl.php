cc0e027403120dfd283a1232f2ec645ded6c5b28
JOSH <53708344+Josh-Algors@users.noreply.github.com>
2022-10-28 00:21:37 +0100

    changes

--- app/Http/Controllers/RegisterCtrl.php
+++ app/Http/Controllers/RegisterCtrl.php
@@ -16,26 +16,26 @@ class RegisterCtrl extends Controller
 
     public function test(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required'
-        ]);
+        // $validator = Validator::make($request->all(), [
+        //     'username' => 'required',
+        //     'email' => 'required|email|unique:users',
+        //     'password' => 'required'
+        // ]);
 
-        if ($validator->fails()) {
-            // send validation failed message
-            $errors = $validator->errors()->all();
-            $error["message"] = $errors[0];
-            $error['status'] = 'ERROR';
-            $error['code'] = 'VALIDATION_ERROR';
-            return response()->json(["error" => $error], 400);
-        }
+        // if ($validator->fails()) {
+        //     // send validation failed message
+        //     $errors = $validator->errors()->all();
+        //     $error["message"] = $errors[0];
+        //     $error['status'] = 'ERROR';
+        //     $error['code'] = 'VALIDATION_ERROR';
+        //     return response()->json(["error" => $error], 400);
+        // }
 
         try{
             $user = new User();
             $user->username = "test";
             $user->email = "test@gmail.com";
-            $user->password = Hash::make($request->password);
+            $user->password = Hash::make("jkjkjkjk");
             $user->save();
         }
         catch(\Throwable $exp)
